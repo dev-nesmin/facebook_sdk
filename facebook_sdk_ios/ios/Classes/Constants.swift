6b7d54118b8231accc56cc962d31afb398de38b0
dev-nesmin <dev.nesmin@gmail.com>
2021-12-14 10:11:12 +0300

    added ios example

---
+++ facebook_sdk_ios/ios/Classes/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  facebook_sdk_ios
+//
+//  Created by Nesrullah Ekinci on 13.12.2021.
+//
+
+import Foundation
---
+++ facebook_sdk_ios/ios/Classes/FacebookSdk.swift
@@ -0,0 +1,8 @@
+//
+//  FacebookSdk.swift
+//  facebook_sdk_ios
+//
+//  Created by Nesrullah Ekinci on 13.12.2021.
+//
+
+import Foundation
